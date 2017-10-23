519a165a9cb36b4c82128af0601c8f73a17a6458
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2017-10-23 13:16:46 +0200

    added docs forlder to publish to gitHub pages

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,9 +9,13 @@ const Article = (props) => {
       <span>{singleArticle.publishedAt}</span>
       <br />
       <span>{singleArticle.author}</span>
+      <br />
       <span>{singleArticle.description}</span>
+      <br />
       <img src={singleArticle.urlToImage} alt=""/>
+      <br />
       <a href={singleArticle.url} className="nice-link" target="_blank">Link here</a>
+      <hr />
     </div>
   )
 }
