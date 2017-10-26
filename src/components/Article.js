ff4cecf873c7e9a67d2d3816fca742bf8be2979e
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2017-10-26 14:08:00 +0200

    styled pretty appearance

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,23 +6,26 @@ const Article = (props) => {
 
   return (
     <div className="single-article">
-      <h3 className="title">
-        <a href={singleArticle.url} className="nice-link" target="_blank">
-        {singleArticle.description}
-        </a>
-      </h3>
-      <div className="main-article">
-        <p className="real-title">{singleArticle.title}</p>
-        <p>{getFunText()}</p>
+      <div className="border-holder">
+        <h3 className="title">
+          <a href={singleArticle.url} className="nice-link" target="_blank">
+          {singleArticle.description}
+          </a>
+        </h3>
+        <div className="main-article">
+          <p className="real-title">{singleArticle.title}</p>
+          <p className="fun-text">{getFunText()}</p>
+        </div>
+        <div className="additional">
+          <a href={singleArticle.url} className="nice-link" target="_blank">Link here</a>
+          <img src={singleArticle.urlToImage} alt=""/>
+          <span className="author">{singleArticle.author}</span>
+        </div>
+        <div className="meta">
+          <span className="date">{singleArticle.publishedAt}</span>
+        </div>
       </div>
-      <div className="additional">
-        <a href={singleArticle.url} className="nice-link" target="_blank">Link here</a>
-        <img src={singleArticle.urlToImage} alt=""/>
-        <span className="author">{singleArticle.author}</span>
-      </div>
-      <div className="meta">
-        <span className="date">{singleArticle.publishedAt}</span>
-      </div>
+      
     </div>
   )
 }
